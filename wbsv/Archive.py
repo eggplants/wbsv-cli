fb225563c0c10e36d9a2901d36d0aa83c0859744
Wiktoria Tomzik <wiktoria.tomzik@vntrs.com>
2020-04-10 22:00:44 +0200

    Since script can't archive anything but a elements, I think it's better to remove imgs, scripts and anything that it an a with a href from the possible scraped items

--- wbsv/Archive.py
+++ wbsv/Archive.py
@@ -68,10 +68,10 @@ def find_uri(url):
                      html_source_charset, 'ignore'
                    )
     uris_misc = BeautifulSoup(html_decoded, "html.parser").findAll(
-                ["a", "img", "script", "link"]
+                ["a"]
               )
     # extract uri link data
-    uris_misc = sum([[i.get("href"), i.get("src")] for i in uris_misc], [])
+    uris_misc = sum([[i.get("href")] for i in uris_misc], [])
     # change "relative" uri into "absolute" one
     uris_misc = {urljoin(url, i) for i in uris_misc}
     # exclude mailto:// or javascript:// ...
@@ -124,7 +124,7 @@ def archive(uri_dic, pageurl, RETRY, ONLYPAGE):
     try:
       for j in range(1,RETRY+1):
         try:
-          print("[%s]: Wait...    "%id_, end="\r")
+          print("[%s]: Wait..."%id_, end="\r")
           archived_uri, exist_flag = capture_or_cache(uri)  # use module of "savepagenow"
           print("[%s"%id_ + "/" + "%d]:"%len(uri_dic),
                 "<%s>"%"NOW" if exist_flag else "PAST", archived_uri)
@@ -146,7 +146,7 @@ def archive(uri_dic, pageurl, RETRY, ONLYPAGE):
       break
 
     except TooManyRedirects:
-      print("[!]API says:TooManyRedirects!", file=sys.stderr)
+      print("[!]API says: TooManyRedirects!", file=sys.stderr)
       print("[!]Need a 1 min break...", file=sys.stderr)
       for t in range(60):
         print("%d/60s" % t,end="\r", file=sys.stderr)
