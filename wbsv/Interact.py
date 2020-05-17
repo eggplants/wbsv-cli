82602fd55989c472b139d579d3defda0693c2908
eggplants <w10776e8w@yahoo.co.jp>
2020-05-17 16:41:32 +0900

    Try to reduce complexity/Interact.py

--- wbsv/Interact.py
+++ wbsv/Interact.py
@@ -3,33 +3,31 @@ from . import Archive
 from . import Find
 
 
+def get_input():
+    try:
+        return input(">>> ")
+    except(EOFError, KeyboardInterrupt):
+        print("\n[+]End.")
+        exit(0)
+
+
 def interactive(opt):
     """Interactive mode like shell."""
+    print("[+]To exit, use CTRL+C or type 'end'")
     while True:
         print("[[Input a target url (ex: https://google.com)]]")
-        try:
-            url = input(">>> ")
-        except(EOFError, KeyboardInterrupt):
-            print("\n[+]End.")
-            break
+        url = get_input()
 
-        # if the input is succeeded ...
-        if Archive.is_url(url):
-            if opt["only-target"]:
-                Archive.archive([url], url, opt["retry"])
-            else:
-                Archive.archive(Find.extract_uri_recursive(
-                    url, opt["level"]),
-                    url, opt["retry"])
-                print("[+]To exit, use CTRL+C or type 'end'")
+        if Archive.is_url(url) and opt["only-target"]:
+            Archive.archive([url], url, opt["retry"])
+
+        elif Archive.is_url(url):
+            dic = Find.extract_uri_recursive(url, opt["level"])
+            Archive.archive(dic, url, opt["retry"])
 
         elif Archive.is_end(url):
             print("[+]End.")
-            break
-
-        elif url == '':
-            continue
+            exit(0)
 
         else:
             print("[!]This input is invalid.", file=sys.stderr)
-            continue
