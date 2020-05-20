648d3682c0747c4f49c3eeaafc73357eb94b05af
Yu Sanagi <yswallow@example.com>
2020-05-20 10:37:12 +0900

    make Interactive OOP

---
+++ wbsv/interactive.py
@@ -0,0 +1,43 @@
+import sys
+import re
+
+class Interactive:
+    def __init__(self, finder, archiver):
+        self.finder = finder
+        self.archiver = archiver
+    
+    def parse_opt(self, opt):
+        self.only_target = opt["only-target"]
+
+
+    def is_end(self, str):
+        """Judge whether str is the fin cmd to quit interactive mode."""
+        return re.compile(r'^(end|exit|exit\(\)|break|bye|:q|finish)$').match(str)
+
+    def is_url(self, url):
+        """Judge whether str is url or not."""
+        return re.compile(r'\A(http|https)://').match(url)
+
+    
+    def run(self):
+        """Interactive mode like shell."""
+        print("[+]To exit, use CTRL+C or type 'end'")
+        
+        while True:
+            print("[[Input a target url (ex: https://google.com)]]")
+            
+            try:
+                str = input(">>> ")
+            except(EOFError, KeyboardInterrupt):
+                print("\n[+]End.")
+                exit(0)
+            
+            if self.is_end(str):
+                print("[+]End.")
+                exit(0)
+            elif self.is_url(str):
+                self.finder.find_and_archive(str, self.archiver)
+                self.finder.print_result()
+            else:
+                print("[!]This input is invalid.", file=sys.stderr)
+            
