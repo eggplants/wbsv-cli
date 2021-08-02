9f33c42ce7d808ce5bfc572f40214af8ab4abd7e
eggplants <w10776e8w@yahoo.co.jp>
2021-08-02 10:38:10 +0900

    lint

--- test_wbsv.py
+++ test_wbsv.py
@@ -4,6 +4,8 @@ from wbsv import crawler, main
 def test1():
     args = main.parse_args(test=['https://example.com', '-l', '1'])
     c = crawler.Clawler(args)
+    urls = {'https://example.com',
+            'https://www.iana.org/domains/example'}
     t = set().union(*c.run_crawl())
-    assert {'https://example.com',
-            'https://www.iana.org/domains/example'} == t
+    if urls != t:
+        raise AssertionError
