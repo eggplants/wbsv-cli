b626f81ad2db15b42d099080615c746ee5e07a6c
eggplants <w10776e8w@yahoo.co.jp>
2022-01-24 01:17:41 +0900

    fix: flake8

--- test/test_wbsv.py
+++ test/test_wbsv.py
@@ -6,36 +6,35 @@ from wbsv import crawler, main
 from wbsv.url_filters import OwnDomainFilter, SchemaFilter
 
 
-def test_schema_filter_should_reject_invalid_schema():
-
+def test_schema_filter_should_reject_invalid_schema() -> None:
     schema_filter = SchemaFilter()
     invalid_schema_url = urlparse("data://test.txt")
 
     assert schema_filter.test_url(invalid_schema_url) is False
 
 
-def test_schema_filter_should_accept_valid_schema():
+def test_schema_filter_should_accept_valid_schema() -> None:
     schema_filter = SchemaFilter()
     valid_schema_url = urlparse("https://example.com")
 
     assert schema_filter.test_url(valid_schema_url) is True
 
 
-def test_own_domain_filter_should_reject_url_with_invalid_domain():
+def test_own_domain_filter_should_reject_url_with_invalid_domain() -> None:
     own_domain_filter = OwnDomainFilter.from_string_urls(["https://example.com"])
     invalid_domain_url = urlparse("https://www.iana.org")
 
     assert own_domain_filter.test_url(invalid_domain_url) is False
 
 
-def test_own_domain_filter_should_accept_url_with_valid_domain():
+def test_own_domain_filter_should_accept_url_with_valid_domain() -> None:
     own_domain_filter = OwnDomainFilter.from_string_urls(["https://example.com"])
     invalid_domain_url = urlparse("https://example.com")
 
     assert own_domain_filter.test_url(invalid_domain_url) is True
 
 
-def test_api_without_parser():
+def test_api_without_parser() -> None:
     args = {
         "urls": ["https://example.com"],
         "own": False,
@@ -49,7 +48,7 @@ def test_api_without_parser():
         raise AssertionError
 
 
-def test1():
+def test1() -> None:
     args = main.parse_args(args_list=["https://example.com", "-l", "1"])
     c = crawler.Crawler.from_parser_args(args)
     urls = {"https://example.com", "https://www.iana.org/domains/example"}
