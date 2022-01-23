aef2c31912a3bf38fe66f4128e5e2e4a116a4f52
eggplants <w10776e8w@yahoo.co.jp>
2022-01-24 00:23:31 +0900

    add: more maintainable configures

--- setup.py
+++ setup.py
@@ -1,20 +1,3 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
-from wbsv import __version__
-
-setup(
-    name="wbsv",
-    version=__version__,
-    description="Throw all URIs in a page on to Wayback Machine from CLI.",
-    description_content_type="",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/eggplants/wbsv-cli",
-    author="eggplants",
-    packages=find_packages(),
-    python_requires=">=3.7",
-    include_package_data=True,
-    license="MIT",
-    install_requires=open("requirements.txt").read().splitlines(),
-    entry_points={"console_scripts": ["wbsv=wbsv.main:main"]},
-)
+setup()
